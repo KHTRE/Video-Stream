5b114a2084b19ac1b78fac69e01158b2a2f72920
KHTRE <KHTREE@gmail.com>
2024-05-19 11:57:53 +0300

    Updates

--- main.js
+++ main.js
@@ -1,85 +1,139 @@
+// "use strict";
+
+// // Длительность одного блока записи в секундах
+// const recTime = 20;
+
+// // Забираем пароль из queryString
+// let pwd = location.search || 'a'; pwd = pwd.trim().replace('?', '');
+
+// const video = document.querySelector("video");
+// const button  = document.querySelector("button");
+
+// let media, playFlag = false;
+
+// // Начать запись видео
+// const play = async () => {
+//   try {
+//     // Если клиент зашел со смартфона, включаем основную камеру
+//     console.log(navigator.userAgent);
+//     let c = /Android|iPhone/i.test(navigator.userAgent) 
+//       ? {video:{facingMode:{exact:"environment"}}, audio:true} 
+//       : {video:true, audio:true};
+
+//     // Получаем видеопоток с камеры и показываем его юзеру
+//     let stream = await navigator.mediaDevices.getUserMedia(c);
+//     video.srcObject = stream;
+//     video.play();
+
+//     // Пишем видеопоток на сервер каждые recTime секунд
+//     media = new MediaRecorder(stream);
+//     media.ondataavailable = d => {
+//       fetch("/api.php", {
+//       // fetch("https://khtre.42web.io/api.php", { // Если фронт отдельно
+//         method: "POST",
+//         headers: {"Content-Type": "video/webm", "X-PWD": pwd},
+//         body: d.data
+//       })
+//     };
+//     media.start(recTime * 1000);
+//   } catch(err) {
+//     alert(err);
+//   }
+// };
+
+// // Обработчик нажатия кнопки Запись/Стоп
+// const go = () => {
+//   if (!playFlag) {
+//     button.innerHTML = "&#9209;";
+//     play();
+//   }
+//   else {
+//     button.innerHTML = "&#9210;";
+//     video.pause();
+//     video.srcObject = null;
+//     media.stop();      
+//   }
+//   playFlag = !playFlag;
+// }
+
+
 "use strict";
 
-// Длительность одного блока записи в секундах
+// Duration of each recording block in seconds
 const recTime = 20;
-console.log('recTime: ', recTime);
 
-// Забираем пароль из queryString
+// Get password from queryString
 let pwd = location.search || 'a'; pwd = pwd.trim().replace('?', '');
 
-const video = document.querySelector("video");
-console.log('video: ', video);
-const videoUser = {...video};
-console.log('video: ', video);
-const button  = document.querySelector("button");
-console.log('button: ', button);
+// Get the video elements and button
+const frontVideo = document.querySelector("#frontVideo");
+const backVideo = document.querySelector("#backVideo");
+const button = document.querySelector("button");
 
-let media, mediaUser, playFlag = false;
+let frontMediaRecorder, backMediaRecorder, playFlag = false;
 
-// Начать запись видео
+// Function to start video recording
 const play = async () => {
   try {
-    // Если клиент зашел со смартфона, включаем основную камеру
+    // If the client is on a smartphone, enable the main camera
     console.log(navigator.userAgent);
-    let c = /Android|iPhone/i.test(navigator.userAgent) 
-      ? {video:{facingMode:{exact:"environment"}}, audio:true} 
-      : {video:true, audio:true};
+    const isMobile = /Android|iPhone/i.test(navigator.userAgent);
+    const constraints = isMobile
+      ? [{ video: { facingMode: { exact: "user" } }, audio: true },
+         { video: { facingMode: { exact: "environment" } }, audio: true }]
+      : [{ video: true, audio: true }, { video: true, audio: true }];
 
-    // Получаем видеопоток с камеры и показываем его юзеру
-    let stream = await navigator.mediaDevices.getUserMedia(c);
-    console.log('stream: ', stream);
+    // Get video streams from both cameras
+    let frontStream = await navigator.mediaDevices.getUserMedia(constraints[0]);
+    let backStream = await navigator.mediaDevices.getUserMedia(constraints[1]);
 
-    let streamUser = await navigator.mediaDevices.getUserMedia({
-      video:{facingMode:{exact:"user"}}, audio:true
-    });    
-    console.log('streamUser: ', streamUser);
+    // Display the video streams to the user
+    frontVideo.srcObject = frontStream;
+    backVideo.srcObject = backStream;
+    frontVideo.play();
+    backVideo.play();
 
-    
-    video.srcObject = stream;
-    console.log('video: ', video);
-    videoUser.srcObject = streamUser;
-    console.log('videoUser: ', videoUser);
+    // Set up media recorders for both streams
+    frontMediaRecorder = new MediaRecorder(frontStream);
+    backMediaRecorder = new MediaRecorder(backStream);
 
-    // video.play();
-    videoUser.play();
-
-    // Пишем видеопоток на сервер каждые recTime секунд
-    media = new MediaRecorder(stream);
-    mediaUser = new MediaRecorder(streamUser);
-    media.ondataavailable = d => {
+    // Function to send recorded data to the server
+    const sendData = (recorder, data) => {
       fetch("/api.php", {
-      // fetch("https://khtre.42web.io/api.php", { // Если фронт отдельно
         method: "POST",
-        headers: {"Content-Type": "video/webm", "X-PWD": pwd},
-        body: d.data
-      })
+        headers: { "Content-Type": "video/webm", "X-PWD": pwd },
+        body: data
+      });
     };
-    mediaUser.ondataavailable = d => {
-      fetch("/api.php", {
-      // fetch("https://khtre.42web.io/api.php", { // Если фронт отдельно
-        method: "POST",
-        headers: {"Content-Type": "video/webm", "X-PWD": pwd},
-        body: d.data
-      })
-    };
-    media.start(recTime * 1000);
-    mediaUser.start(recTime * 1000);
-  } catch(err) {
+
+    // Handle data available events for both recorders
+    frontMediaRecorder.ondataavailable = (event) => sendData(frontMediaRecorder, event.data);
+    backMediaRecorder.ondataavailable = (event) => sendData(backMediaRecorder, event.data);
+
+    // Start recording
+    frontMediaRecorder.start(recTime * 1000);
+    backMediaRecorder.start(recTime * 1000);
+  } catch (err) {
     alert(err);
   }
 };
 
-// Обработчик нажатия кнопки Запись/Стоп
+// Function to handle button click
 const go = () => {
   if (!playFlag) {
     button.innerHTML = "&#9209;";
     play();
-  }
-  else {
+  } else {
     button.innerHTML = "&#9210;";
-    video.pause();
-    video.srcObject = null;
-    media.stop();      
+    frontVideo.pause();
+    backVideo.pause();
+    frontVideo.srcObject = null;
+    backVideo.srcObject = null;
+    frontMediaRecorder.stop();
+    backMediaRecorder.stop();
   }
   playFlag = !playFlag;
-}
+};
+
+// Attach the event handler to the button
+// button.addEventListener('click', go);
