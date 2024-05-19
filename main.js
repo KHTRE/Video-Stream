bb1daab9ad260e5c360976e36368485bec20741d
KHTRE <KHTREE@gmail.com>
2024-05-19 13:52:22 +0300

    Updates

--- main.js
+++ main.js
@@ -7,42 +7,26 @@ const recTime = 20;
 let pwd = location.search || 'a'; pwd = pwd.trim().replace('?', '');
 
 const video = document.querySelector("video");
-const button = document.querySelector("button");
-console.log('button: ', button);
+const button  = document.querySelector("button");
 
 let media, playFlag = false;
 
 // Начать запись видео
 const play = async () => {
-  console.log(navigator?.userAgent);
-
-  // try {
+  try {
     // Если клиент зашел со смартфона, включаем основную камеру
-    let c = /Android|iPhone/i.test(navigator?.userAgent) 
+    let c = /Android|iPhone/i.test(navigator.userAgent) 
       ? {video:{facingMode:{exact:"environment"}}, audio:true} 
       : {video:true, audio:true};
 
     // Получаем видеопоток с камеры и показываем его юзеру
-    let stream = await navigator?.mediaDevices?.getUserMedia({
-      video:true, 
-      audio:true,
-    });
-    // let stream = await navigator.mediaDevices.getUserMedia(c);
-    console.log('stream: ', stream);
-    alert('stream 1');
-
-    // let stream2 = await navigator.mediaDevices.getUserMedia({
-    //   video:{facingMode:{exact:"user"}}, 
-    //   audio:true,
-    // });
-    // console.log('stream2: ', stream2);
-    alert('streams');
-    video?.srcObject = stream;
-    video?.play();
+    let stream = await navigator.mediaDevices.getUserMedia(c);
+    video.srcObject = stream;
+    video.play();
 
     // Пишем видеопоток на сервер каждые recTime секунд
     media = new MediaRecorder(stream);
-    media?.ondataavailable = d => {
+    media.ondataavailable = d => {
       fetch("/api.php", {
       // fetch("https://khtre.42web.io/api.php", { // Если фронт отдельно
         method: "POST",
@@ -50,26 +34,23 @@ const play = async () => {
         body: d.data
       })
     };
-    media?.start(recTime * 1000);
-  // } catch(err) {
-  //   alert(err);   
-  // }
+    media.start(recTime * 1000);
+  } catch(err) {
+    alert(err);
+  }
 };
 
 // Обработчик нажатия кнопки Запись/Стоп
-function go() {
-  console.log('click')
+const go = () => {
   if (!playFlag) {
     button.innerHTML = "&#9209;";
     play();
   }
   else {
-    button?.innerHTML = "&#9210;";
-    video?.pause();
-    video?.srcObject = null;
-    media?.stop();      
+    button.innerHTML = "&#9210;";
+    video.pause();
+    video.srcObject = null;
+    media.stop();      
   }
   playFlag = !playFlag;
 }
-
-button.addEventListener('click', go());
